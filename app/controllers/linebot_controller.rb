782cda38dd450fc5f303888ba5f56acb80fb83d1
Katsuyoshi Ito <kito@itosoft.com>
2022-02-20 22:23:01 +0900

    replace with line sample

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -7,6 +7,32 @@ class LinebotController < ApplicationController
 
   def callback
     body = request.body.read
+
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      error 400 do 'Bad Request' end
+    end
+  
+    events = client.parse_events_from(body)
+    events.each do |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+          client.reply_message(event['replyToken'], message)
+        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+          response = client.get_message_content(event.message['id'])
+          tf = Tempfile.open("content")
+          tf.write(response.body)
+        end
+      end
+    end
+=begin
+    body = request.body.read
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
       error 400 do 'Bad Request' end
@@ -26,6 +52,7 @@ class LinebotController < ApplicationController
       end
       client.reply_message(event['replyToken'], message)
     end
+=end
     head :ok
   end
 
